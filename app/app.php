1f7d433c8ced919b562e5f67dc9b9f82eadf86a8
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-02-17 14:03:25 -0800

    add twig routes and template for display of results

--- app/app.php
+++ app/app.php
@@ -10,7 +10,14 @@
     ));
 
     $app->get("/", function() use ($app) {
-        
+        return $app["twig"]->render("home.html.twig");
+    });
+
+    $app->get("/results", function() use ($app) {
+        $new_FindAndReplace = new FindAndReplace($_GET["input_string"], $_GET["word_to_replace"], $_GET["replacement_word"]);
+        $result = $new_FindAndReplace->replaceWord($_GET["input_string"], $_GET["word_to_replace"], $_GET["replacement_word"]);
+    
+        return $app["twig"]->render("home.html.twig", array("newresult" => $result, "originalinput" => $new_FindAndReplace));
     });
 
     return $app;
