344f768e9335ec87e65019129cb52c158ae76f84
jared beckler and torrence stratton <jared.beckler@gmail.com>
2016-02-17 13:33:59 -0800

    add constructor/setters/getter function

--- app/app.php
+++ app/app.php
@@ -9,7 +9,9 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
 
-
+    $app->get("/", function() use ($app) {
+        
+    });
 
     return $app;
  ?>
--- src/FindAndReplace.php
+++ src/FindAndReplace.php
@@ -1,6 +1,47 @@
 <?php
     class FindAndReplace {
 
+        private $inputString;
+        private $wordToReplace;
+        private $replacementWord;
+
+        function __construct($new_inputString, $new_wordToReplace, $new_replacementWord)
+        {
+            $this->inputString = $new_inputString;
+            $this->wordToReplace = $new_wordToReplace;
+            $this->replacementWord = $new_replacementWord;
+        }
+
+        function setInputString($new_inputString)
+        {
+            $this->inputString = $new_inputString;
+        }
+
+        function setWordToReplace($new_wordToReplace)
+        {
+            $this->wordToReplace = $new_wordToReplace;
+        }
+
+        function setReplacementWord($new_replacementWord)
+        {
+            $this->replacementWord = $new_replacementWord;
+        }
+
+        function getInputString()
+        {
+            return $this->inputString;
+        }
+
+        function getWordToReplace()
+        {
+            return $this->wordToReplace;
+        }
+
+        function getReplacementWord()
+        {
+            return $this->replacementWord;
+        }
+
         function replaceWord($inputString, $wordToReplace, $replacementWord)
         {
             $onlyAlphaInputString = preg_replace("/[^a-z0-9]/i", " ", $inputString);
