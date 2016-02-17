7de937cd81c1f4d201bc1e2ee2384e8275a88baf
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-02-17 10:36:45 -0800

    add initial project files and structure

---
+++ src/FindAndReplace.php
@@ -0,0 +1,6 @@
+<?php
+    class FindAndReplace {
+
+
+    }
+?>
---
+++ tests/FindAndReplaceTest.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "src/FindAndReplace.php";
+
+    class FindAndReplaceTest extends PHPUnit_Framework_TestCase {
+
+
+    }
+?>
